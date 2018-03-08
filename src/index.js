4d2ace191a971a5d7ff0a56bd158e89fbebec34c
David O'Regan <doregan@vsware.ie>
2018-03-08 15:55:26 +0000

    [core-story] Create finally method and index imports

---
+++ src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+"use strict";
+
+import * as core from "./core";
+import * as done from "./done";
+import * as fin from "./finally";
+import * as es6Ext from "./es6";
+import * as synchronous from "./synchronous";
