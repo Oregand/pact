edfbb686774a3aea21107d93e3cf2b126f9c4773
David O'Regan <doregan@vsware.ie>
2018-03-08 15:48:45 +0000

    [core-story] Create dome method which is the same as `.then` but it doesnt return a promise and the errors and rethrown so we can log them

--- src/done.js
+++ src/done.js
@@ -1,1 +1,15 @@
 "use strict";
+
+import { Promise } from "./core";
+
+export default Promise;
+
+Promise.prototype.done = function(onFufilled, onRejected) {
+  const self = arguments.length ? this.then.apply(this, arguments) : this;
+
+  self.then(null, function(err) {
+    setTimeout(function() {
+      throw err;
+    }, 0);
+  });
+};
